80340a310ddcc9d46828d137b6e60334c9a34c13
ilya <soten58@yandex.ru>
2016-08-17 12:24:31 +0300

    connected passbooks with users

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,18 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  validates :first_name, :last_name,
+            presence: true
   has_one :passbook
+
+  after_create :new_passbook
+
+  def new_passbook
+    @pass = Passbook.new
+    @pass.number = @pass.id
+    @pass.currency = 0
+    @pass.user_id = User.last.id
+    @pass.save
+  end
+
 end
